f67eb8a473dd47eed741a782e8ad276780cb4ea1
karolis.vas@gmail.com <karolis.vas@gmail.com>
2026-01-19 21:38:39 +0200

    Add char sort and nicer print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_characters
+from stats import sort_dictionary
 
 def get_book_text(file_path):
     try:
@@ -12,5 +13,14 @@ def get_book_text(file_path):
 if __name__ == '__main__':
     text = get_book_text("books/frankenstein.txt")
     count_of_words = count_words(text)
+    counted_chars = count_characters(text)
+    sorted_chars = sort_dictionary(counted_chars)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {len(count_of_words)} total words")
-    print(count_characters(text))
+    print("--------- Character Count -------")
+    for char in sorted_chars:
+        print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,6 +1,5 @@
 import string
 
-
 def count_words(text):
     return text.split()
 
@@ -13,3 +12,13 @@ def count_characters(text):
         else:
             list_of_chars[lower_case] += 1
     return list_of_chars
+
+def sort_dictionary(dictionary):
+    list_of_chars = []
+    for d in dictionary:
+        list_of_chars.append({"char": d, "num": dictionary[d]})
+    list_of_chars.sort(reverse=True, key=sort_on)
+    return list_of_chars
+
+def sort_on(item):
+    return item["num"]
