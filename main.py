7c57853de2cc1c0b0447ace8c9edbcb2b0450842
karolis.vas@gmail.com <karolis.vas@gmail.com>
2026-01-19 21:49:04 +0200

    Add arguments to python

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import count_words
 from stats import count_characters
 from stats import sort_dictionary
+import sys
 
 def get_book_text(file_path):
     try:
@@ -8,16 +9,22 @@ def get_book_text(file_path):
             book_text = f.read()
         return book_text
     except FileNotFoundError:
-        print("frankenstein book not found")
+        print("book not found")
+        sys.exit(1)
 
 if __name__ == '__main__':
-    text = get_book_text("books/frankenstein.txt")
+
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+
+    text = get_book_text(sys.argv[1])
     count_of_words = count_words(text)
     counted_chars = count_characters(text)
     sorted_chars = sort_dictionary(counted_chars)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {len(count_of_words)} total words")
     print("--------- Character Count -------")
