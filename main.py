9a185e7c04ac4549791d2a9fccc8ed526124235d
karolis.vas@gmail.com <karolis.vas@gmail.com>
2026-01-19 18:15:59 +0200

    add exception handling

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def get_book_text(file_path):
-    with open(file_path, 'r') as f:
-        return f.read()
-
+    try:
+        with open(file_path) as f:
+            book_text = f.read()
+        return book_text
+    except FileNotFoundError:
+        print("frankenstein book not found")
 
 if __name__ == '__main__':
     book_path = "books/frankenstein.txt"
