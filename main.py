258abfd38106ad0fdfef47c26fe0f4c09cbbc98d
karolis.vas@gmail.com <karolis.vas@gmail.com>
2026-01-19 18:10:33 +0200

    Create file read

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
+
+
+if __name__ == '__main__':
+    book_path = "books/frankenstein.txt"
+    print(get_book_text(book_path))
